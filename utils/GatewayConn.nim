9512c97c9ac38b848738c8444856f0a4fc9c4c78
shdShadow <simomosino@gmail.com>
2025-04-05 02:47:03 +0100

    Implemented Heartbeat

--- utils/GatewayConn.nim
+++ utils/GatewayConn.nim
@@ -1,30 +1,43 @@
-import ws, std/async, json
+import ws, std/async, json, std/asyncdispatch
 import GatewayEvent
+import os
 # GLOBALS
 var first_event: GatewayEvent = nil
 var discord_websocket: WebSocket
 # PROTOTYPES
 proc ReceiveMessage(): Future[void] {.async.}
 proc HandleMessage(message: system.string): Future[void] {.async.}
+proc MaintainHeartbeat(): Future[void] {.async.}
 proc RunConnection*(): Future[void] {.async.}
 
 proc RunConnection*(): Future[void] {.async.} =
     discord_websocket = await newWebSocket("wss://gateway.discord.gg/?v=10&encoding=json")
-    await ReceiveMessage()
+    await all(ReceiveMessage(), MaintainHeartbeat())
 
 proc ReceiveMessage(): Future[void] {.async.} =
-    HandleMessage(await discord_websocket.receiveStrPacket())
+    while true:
+        echo "Im receving a message"
+        await HandleMessage(await discord_websocket.receiveStrPacket())
+    
 
 proc HandleMessage(message: string): Future[void] {.async.} =
-    let temp =  parseJson(message)
-    echo temp.pretty()
-
+    echo "I have handled a message"
+    echo message
     let hello = convertJson(message)
     if first_event == nil :
         first_event = hello
         echo first_event.d.heartbeat_interval
 
 proc MaintainHeartbeat(): Future[void] {.async.} =
-    if first_event == null:
-        return
+    while true:
+        echo "Im trying to send an Heartbeat"
+        if first_event != nil:
+            discard discord_websocket.send(first_event.forgeJsonString())
+            echo "Sent Heartbeat!"
+            sleep(first_event.d.heartbeat_interval)
+            echo "Sent Heartbeat"
+        else:
+            echo "I couldn't the event is null"
+
+
     
--- utils/GatewayEvent.nim
+++ utils/GatewayEvent.nim
@@ -11,4 +11,16 @@ proc convertJson*(json: string): GatewayEvent =
     var json_object = parseJson(json)
     return GatewayEvent(op: json_object["op"].getInt(), s:json_object["s"].getInt(), d: GatewayData(heartbeat_interval: json_object["d"]["heartbeat_interval"].getInt()), t: json_object["t"].getStr())
 
+proc forgeJsonString*(event: GatewayEvent): string = 
+    var sequence = ""
+    if event.s == 0:
+        sequence = "null"
+    else: 
+        sequence = $event.s
+    let json_string = %*
+        {"op": 1, "s": sequence}
+    return json_string.pretty(0)
+
+
+
 
